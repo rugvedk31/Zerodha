ac76f06c2469b482eae91e85e6f89c7efb3c6c60
rugvedk31 <rugvedku253@gmail.com>
2025-07-02 17:33:27 +0530

    hero

--- frontend/src/landing_pages/home/Hero.js
+++ frontend/src/landing_pages/home/Hero.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+    return (
+        <div className='container p-5'>
+            <div className='row text-center'>
+                <img
+                    src="media/images/homehero.png"
+                    alt="Hero"
+                    className='mb-5'
+                />
+                <h1 className='mt-5'>Invest in everything</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%", margin:"0 auto"}}>Sign up</button>
+            </div>
+        </div>
+    );
 }
 
 export default Hero;
