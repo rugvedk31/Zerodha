a343c3ce30457d721dec87c8ca1c596469db5520
rugvedk31 <rugvedku253@gmail.com>
2025-07-02 17:34:52 +0530

    openAccount

--- frontend/src/landing_pages/OpenAccount.js
+++ frontend/src/landing_pages/OpenAccount.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function OpenAccount() {
     return ( 
-        <h1>OpenAccount</h1>
+        <div className='container'>
+            <div className='row text-center'>
+                <h1 className='mb-3 fs-2'>Open a Zerodha account</h1>
+                <p className='text-muted'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+
+                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%", margin:"0 auto"}}>Sign up</button>
+            </div>
+        </div>
      );
 }
 
