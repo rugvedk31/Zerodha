be54410bb7f413b2783a7a4fce2f47fcad135eb6
rugvedk31 <rugvedku253@gmail.com>
2025-07-02 17:34:11 +0530

    stats

--- frontend/src/landing_pages/home/Stats.js
+++ frontend/src/landing_pages/home/Stats.js
@@ -2,7 +2,37 @@ import React from 'react'
 
 function Stats() {
     return ( 
-        <h1>Stats</h1>
+        <div className='container p-5'>
+            <div  className='row p-5'>
+                <div className='col-lg-6 col-sm-12 p-5'>
+                    <h1 className='fs-2 mb-5'>Trust with confidence</h1>
+
+                    <h2 className='fs-4'>Customer-first always</h2>
+                    <p className='text-muted'>That's why 1.6+ crore customers trust Zerodha with ~ ₹6 lakh crores of equity investments and contribute to 15% of daily retail exchange volumes in India.</p>
+                    
+                    <h2 className='fs-4'>No spam or gimmicks
+</h2>
+                    <p className='text-muted'>No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like. Our philosophies.</p>
+
+                    <h2 className='fs-4'>The Zerodha universe</h2>
+                    <p className='text-muted'>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
+                    
+                    <h2 className='fs-4'>Do better with money</h2>
+                    <p className='text-muted'>With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.
+
+</p>
+
+                </div>
+                <div className='col-lg-6 col-sm-12 p-5'>
+                    <img src="media\images\ecosystem.png" alt="ecosystem" style={{width:"90%"}} />
+
+                    <div>
+                        <a href="" className='mx-5' style={{textDecoration:"none"}}>Explore our products <i class="fa-solid fa-arrow-right"></i></a>
+                        <a href="" className='mx-5' style={{textDecoration:"none"}}>Try Kite demo <i class="fa-solid fa-arrow-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
      );
 }
 
