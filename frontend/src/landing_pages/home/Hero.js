c79f8a45f40f4c9c95d361d36ce2999045478b1a
rugvedk31 <rugvedku253@gmail.com>
2025-07-04 00:25:57 +0530

    hero

--- frontend/src/landing_pages/home/Hero.js
+++ frontend/src/landing_pages/home/Hero.js
@@ -3,15 +3,29 @@ import React from 'react'
 function Hero() {
     return (
         <div className='container p-5'>
-            <div className='row text-center'>
-                <img
-                    src="media/images/homehero.png"
-                    alt="Hero"
-                    className='mb-5'
-                />
-                <h1 className='mt-5'>Invest in everything</h1>
-                <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%", margin:"0 auto"}}>Sign up</button>
+            <div className='row text-center justify-content-center'>
+                <div className="col-12">
+                    <img
+                        src="media/images/homehero.png"
+                        alt="Hero"
+                        className='mb-5 img-fluid mt-3'
+                        style={{ maxWidth: '100%', height: 'auto' }}
+                    />
+                </div>
+
+                <div className="col-12">
+                    <h1 className='mt-3'>Invest in everything</h1>
+                    <p className="px-3">Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                </div>
+
+                <div className="col-12">
+                    <button
+                        className='p-2 btn btn-primary fs-5 mb-5'
+                        style={{ width: "100%", maxWidth: "250px", margin: "0 auto" }}
+                    >
+                        Sign up
+                    </button>
+                </div>
             </div>
         </div>
     );
