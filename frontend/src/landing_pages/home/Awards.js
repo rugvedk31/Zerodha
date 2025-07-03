e8c58b708b978162d3d7eaa1a818ec2fa5e7568d
rugvedk31 <rugvedku253@gmail.com>
2025-07-04 00:26:13 +0530

    awards

--- frontend/src/landing_pages/home/Awards.js
+++ frontend/src/landing_pages/home/Awards.js
@@ -4,12 +4,19 @@ function Awards() {
     return (
         <div className='container mt-5'>
             <div className='row'>
-                <div className='col-lg-6 col-sm-12 p-5'>
-                    <img src="media/images/largestBroker.svg" alt="largestBroker" />
+                <div className='col-lg-6 col-sm-12 p-5 text-center'>
+                    <img
+                        src="media/images/largestBroker.svg"
+                        alt="largestBroker"
+                        className='img-fluid'
+                    />
                 </div>
+
                 <div className='col-lg-6 col-sm-12 p-5 mt-5'>
                     <h1>Largest stock broker in India</h1>
-                    <p className='mb-5'>2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+                    <p className='mb-5'>
+                        2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:
+                    </p>
 
                     <div className='row'>
                         <div className='col-lg-6 col-sm-12'>
@@ -22,13 +29,21 @@ function Awards() {
 
                         <div className='col-lg-6 col-sm-12'>
                             <ul>
-                                <li>Stocks & IPOS</li>
+                                <li>Stocks & IPOs</li>
                                 <li>Direct mutual funds</li>
                                 <li>Bonds and Govt. Securities</li>
                             </ul>
                         </div>
                     </div>
-                    <img src="media\images\pressLogos.png" alt="pressLogo"style={{width : "90%"}} />
+
+                    <div className="text-center mt-4">
+                        <img
+                            src="media/images/pressLogos.png"
+                            alt="pressLogo"
+                            className='img-fluid'
+                            style={{ width: "90%" }}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
