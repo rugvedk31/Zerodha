e82c572f01ab7a17d602467024cde74e845cdb9e
rugvedk31 <rugvedku253@gmail.com>
2025-07-07 19:11:38 +0530

    router setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import HomePage from './landing_pages/home/HomePage';
 
+//Router imports
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Page component imports
+import Navbar from './landing_pages/Navbar';
+import HomePage from './landing_pages/home/HomePage';
+// ...existing code...
+import AboutPage from './landing_pages/about/AboutPage';
+// ...existing code...
+// import Product from './products/Product';
+// import Pricing from './home/Pricing';
+// import Support from './support/Support';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HomePage />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        {/* <Route path="/product" element={<Product />} /> */}
+        {/* <Route path="/pricing" element={<Pricing />} /> */}
+        {/* <Route path="/support" element={<Support />} /> */}
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
